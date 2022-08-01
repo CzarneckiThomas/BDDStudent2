1e1a9548712e7c7cc533e72128bd1ab9c06ddb04
ThomasCza <czarnecki.thomas@orange.fr>
2022-08-01 16:44:52 +0200

    Ajout de DataFixtures
    ok

--- src/DataFixtures/TestFixtures.php
+++ src/DataFixtures/TestFixtures.php
@@ -32,13 +32,59 @@ class TestFixtures extends Fixture
 
     public function loadSchoolYears(ObjectManager $manager, FakerGenerator $faker): void
     {
-        $SchoolYears = [
-            '2000',
-            '2001',
-            '2002',
+        $schoolYearDatas = [
+            [
+                'name' => '2022',
+                'started_at' => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-09-01 09:00:00'),
+                'finished_at' => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-012-01 09:00:00'),
+            ],
+            [
+                'name' => '2023',
+                'started_at' => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2023-09-01 09:00:00'),
+                'finished_at' => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2023-012-01 09:00:00'),
+            ],
+            [
+                'name' => '2024',
+                'started_at' => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2024-09-01 09:00:00'),
+                'finished_at' => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2024-012-01 09:00:00'),
+            ]
         ];
+
+        foreach ($schoolYearDatas as $schoolYearData) {
+            $schoolYear = new SchoolYear();
+            $schoolYear->setName($schoolYearData['name']);
+            $schoolYear->setStartedAt($schoolYearData['started_at']);
+            $schoolYear->setFinishedAt($schoolYearData['finished_at']);
+
+            $manager->persist($schoolYear);
+
+        }
 
         $manager->flush();
     }
+
+    // public function loadStudents(ObjectManager $manager, FakerGenerator $faker): void
+    // {
+    //     $schoolYearDatas = [
+    //         [
+    //             'firstname' => 'Thomas',
+    //             'lastname' => 'Czarnecki',
+    //             'email' => 'exemple@orange.fr',
+    //         ],
+    //         [
+    //             'firstname' => 'Marion',
+    //             'lastname' => 'Czar',
+    //             'email' => 'exemple@free.fr',
+    //         ],
+    //         [
+    //             'firstname' => 'Tim',
+    //             'lastname' => 'Czar',
+    //             'email' => 'exemple@laposte.fr',
+    //         ],
+           
+    //     ];
+
+    //     $manager->flush();
+    // }
 
 }
